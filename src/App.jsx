64182dcc4ad745ee7acdc6e08e9c132e852c360c
kajrbae-create <kajrbae@gmail.com>
2026-09-15 16:27:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import ClientBlockListener from "./components/ClientBlockListener";
 import { getCurrentUserId } from "./utils/orderId";
 
 //export const api_route = "http://localhost:8080";
-export const api_route = "https://bcare-ak-last-se4-production.up.railway.app";
+export const api_route = "https://bcare-ak-last-se4-production-293e.up.railway.app";
 export const socket = io(api_route);
 
 export function getKeysWithTrueValue(obj) {
